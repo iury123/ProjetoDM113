d56afb3c3fd97bad2d2740044c7e9baf36231d23
iurymig.sht@gmail.com <iurymig.sht@gmail.com>
2019-07-11 19:48:13 -0300

    Criando servico de estoques

---
+++ ServicoEstoque/IServicoEstoque.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Estoques
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IServicoEstoque
+    {
+
+        [OperationContract]
+        List<string> ListarProdutos();
+        [OperationContract]
+        bool IncluirProduto(Produto produto);
+        [OperationContract]
+        bool RemoverProduto(string NumeroProduto);
+        [OperationContract]
+        int ConsultarEstoque(string NumeroProduto);
+        [OperationContract]
+        bool AdicionarEstoque(string NumeroProduto, int Quantidade);
+        [OperationContract]
+        bool RemoverEstoque(string NumeroProduto, string Quantidade);
+        [OperationContract]
+        Produto VerProduto(string NumeroProduto);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Produto
+    {
+        [DataMember]
+        public string NumeroProduto { get; set; }
+
+        [DataMember]
+        public string NomeProduto { get; set; }
+
+        [DataMember]
+        public string DescricaoProduto { get; set; }
+
+        [DataMember]
+        public int EstoqueProduto { get; set; }
+    }
+}
---
+++ ServicoEstoque/ServicoEstoque.svc.cs
@@ -0,0 +1,66 @@
+﻿using EstoqueEntityModel;
+using Estoques;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Estoques
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ServicoEstoque : IServicoEstoque
+    {
+        public bool AdicionarEstoque(string NumeroProduto, int Quantidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ConsultarEstoque(string NumeroProduto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IncluirProduto(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> ListarProdutos()
+        {
+            List<string> produtos = new List<string>();
+            try
+            {
+                using(ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    List<ProdutoEstoque> produtoEstoques = database.ProdutoEstoques.ToList();
+                    foreach(ProdutoEstoque estoque in produtoEstoques)
+                    {
+                        produtos.Add(estoque.NomeProduto);
+                    }
+                }
+            } catch {}
+            return produtos;
+        }
+
+        public bool RemoverEstoque(string NumeroProduto, string Quantidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoverProduto(string NumeroProduto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto VerProduto(string NumeroProduto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
