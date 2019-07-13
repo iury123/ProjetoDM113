afac8190742e74ba9e0ffa337372adaf0c05ff32
iurymig.sht@gmail.com <iurymig.sht@gmail.com>
2019-07-13 16:16:34 -0300

    Criando cliente 02

---
+++ ProvedorServicosVendasClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using ProvedorServicosVendasClient.ServicoEstoque;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvedorServicosVendasClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicoEstoqueClient proxy = new ServicoEstoqueClient("WS2007HttpBinding_IServicoEstoque");
+            // Test the operations in the service
+            // Obtain a list of all products
+            Console.WriteLine("Test 1: List all products");
+            int value = proxy.ConsultarEstoque("2000");
+            Console.WriteLine("VALUE {0}", value);
+            Console.WriteLine();
+            // Disconnect from the service
+            proxy.Close();
+            Console.WriteLine("Press ENTER to finish");
+            Console.ReadLine();
+        }
+    }
+}
